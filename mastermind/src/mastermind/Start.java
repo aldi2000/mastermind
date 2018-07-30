40ff84ce09828010da8b171838c784abf007c1ac
benhi <benhi@benjamins-mini.fritz.box>
2018-07-30 22:06:07 +0200

    gittest

--- mastermind/src/mastermind/Start.java
+++ mastermind/src/mastermind/Start.java
@@ -4,7 +4,7 @@ public class Start {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println ("Mastermind v1");
+		System.out.println ("Mastermind v1.1");
 		Mind mind = new Mind();
 		mind.start();
 		
